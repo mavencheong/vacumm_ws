11a989fcbd7f8bbf4ca24ef1114faaedcceb7267
Maven Cheong <mavencko@maxis.com.my>
2022-02-02 23:03:03 +0800

    update

--- src/vacumm_arduino/src/vacumm_robot/vacumm_robot.ino
+++ src/vacumm_arduino/src/vacumm_robot/vacumm_robot.ino
@@ -5,6 +5,8 @@
 #include <vacumm_hardware/WheelCmd.h>
 #include <vacumm_hardware/WheelState.h>
 
+#define ROS_SERIAL false
+
 #define RIGHT_MOTOR_PIN_A 22
 #define RIGHT_MOTOR_PIN_B 23
 #define RIGHT_MOTOR_ENCODER_A 12
@@ -42,7 +44,7 @@ volatile long left_motor_rpm = 0;
 float left_motor_act_vel = 0;
 float left_motor_vel = 0.0;
 int left_motor_pos = 0;
-long left_motor_curr_pulse =0;
+long left_motor_curr_pulse = 0;
 long left_motor_pre_pulse = 0;
 int left_motor_speed = 0;
 
@@ -77,8 +79,16 @@ ros::Publisher wheel_state_pub("/vacumm/wheel_state", &wheel_state);
 
 
 void wheel_cmd_callback(const vacumm_hardware::WheelCmd& wheelCmd) {
+
+  char result[8];
+  char log_msg [100];
+
+  dtostrf(wheelCmd.vel[0], 6, 2, result);
+  sprintf(log_msg, "wheelCmd.vel[0] =%s", result);
+  nh.loginfo(log_msg);
   left_motor_vel = wheelCmd.vel[0];
   right_motor_vel = wheelCmd.vel[1];
+
 }
 
 ros::Subscriber<vacumm_hardware::WheelCmd> wheel_cmd_sub("/vacumm/wheel_cmd", &wheel_cmd_callback);
@@ -142,6 +152,10 @@ void IRAM_ATTR left_motor_encoder_callback() {
 
 
 void setup() {
+
+  if (!ROS_SERIAL) {
+    Serial.begin(115200);
+  }
   pinMode(LED_PIN, OUTPUT);
   flashLED(3);
 
@@ -167,16 +181,24 @@ void setup() {
   right_motor_pulse = 0;
   right_motor_pre_pulse = 0;
 
-  nh.getHardware()->setBaud(115200);
-  nh.initNode();
-  nh.advertise(wheel_state_pub);
-  nh.subscribe(wheel_cmd_sub);
+
+  if (ROS_SERIAL) {
+    nh.getHardware()->setBaud(115200);
+    nh.initNode();
+    nh.advertise(wheel_state_pub);
+    nh.subscribe(wheel_cmd_sub);
+  }
+
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  nh.spinOnce();
+  if (ROS_SERIAL) {
+    nh.spinOnce();
+  } else {
+    readCommand();
+  }
 
   currentMillis = millis();
 
@@ -199,10 +221,10 @@ void loop() {
 
     left_motor_act_vel = left_input / VELOCITY_TO_PULSE_MULTIPLIER ;
     right_motor_act_vel = right_input / VELOCITY_TO_PULSE_MULTIPLIER;
-  
+
     left_motor_pos = left_input * RADS_PER_TICK_COUNT;
     right_motor_pos = right_input * RADS_PER_TICK_COUNT;
-    
+
 
     leftMotorPID.Compute();
     rightMotorPID.Compute();
@@ -212,8 +234,8 @@ void loop() {
 
     drive(left_motor_speed, right_motor_speed);
 
-    
-    if(no_comm_loop > NO_COMM_MAX){
+
+    if (no_comm_loop > NO_COMM_MAX) {
       left_motor_vel = 0;
       right_motor_vel = 0;
       drive(0, 0);
@@ -221,16 +243,19 @@ void loop() {
 
     no_comm_loop++;
 
-    if (no_comm_loop == 65535){
+    if (no_comm_loop == 65535) {
       no_comm_loop = NO_COMM_MAX;
     }
 
-    publish_wheel_state();
+    if (ROS_SERIAL) {
+      publish_wheel_state();
+    }
+
   }
 }
 
 
-void publish_wheel_state(){
+void publish_wheel_state() {
   wheel_state.vel[0] = left_motor_act_vel;
   wheel_state.vel[1] = right_motor_act_vel;
   wheel_state.pos[0] = left_motor_pos;
@@ -238,4 +263,21 @@ void publish_wheel_state(){
 
   wheel_state_pub.publish(&wheel_state);
   nh.spinOnce();
+}
+
+
+void readCommand(){
+  if (Serial.available()){
+    String command = Serial.readStringUntil('\n');
+
+    Serial.printf("Command received %s \n", command);
+     if (command.substring(0, 1).equals("v")){
+      int commaIndex = command.indexOf(',');
+      double x = command.substring(1, commaIndex).toDouble();
+      double z = command.substring(commaIndex + 1, command.length()).toDouble();
+      no_comm_loop = 0;
+      left_motor_vel = x;
+      right_motor_vel = z;
+    }
+  }
 }
