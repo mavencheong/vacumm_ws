59eb318b68dc888727a80325c48a877fb5200b49
maven <mavencheong@gmail.com>
2022-02-23 22:23:31 +0800

    update

--- src/vacumm_arduino/src/vacumm_hardware/VacummDiag.h
+++ src/vacumm_arduino/src/vacumm_hardware/VacummDiag.h
@@ -24,6 +24,10 @@ namespace vacumm_hardware
       _left_output_type left_output;
       typedef double _right_output_type;
       _right_output_type right_output;
+      typedef double _left_pulse_type;
+      _left_pulse_type left_pulse;
+      typedef double _right_pulse_type;
+      _right_pulse_type right_pulse;
 
     VacummDiag():
       left_setpoint(0),
@@ -31,7 +35,9 @@ namespace vacumm_hardware
       left_input(0),
       right_input(0),
       left_output(0),
-      right_output(0)
+      right_output(0),
+      left_pulse(0),
+      right_pulse(0)
     {
     }
 
@@ -122,6 +128,34 @@ namespace vacumm_hardware
       *(outbuffer + offset + 6) = (u_right_output.base >> (8 * 6)) & 0xFF;
       *(outbuffer + offset + 7) = (u_right_output.base >> (8 * 7)) & 0xFF;
       offset += sizeof(this->right_output);
+      union {
+        double real;
+        uint64_t base;
+      } u_left_pulse;
+      u_left_pulse.real = this->left_pulse;
+      *(outbuffer + offset + 0) = (u_left_pulse.base >> (8 * 0)) & 0xFF;
+      *(outbuffer + offset + 1) = (u_left_pulse.base >> (8 * 1)) & 0xFF;
+      *(outbuffer + offset + 2) = (u_left_pulse.base >> (8 * 2)) & 0xFF;
+      *(outbuffer + offset + 3) = (u_left_pulse.base >> (8 * 3)) & 0xFF;
+      *(outbuffer + offset + 4) = (u_left_pulse.base >> (8 * 4)) & 0xFF;
+      *(outbuffer + offset + 5) = (u_left_pulse.base >> (8 * 5)) & 0xFF;
+      *(outbuffer + offset + 6) = (u_left_pulse.base >> (8 * 6)) & 0xFF;
+      *(outbuffer + offset + 7) = (u_left_pulse.base >> (8 * 7)) & 0xFF;
+      offset += sizeof(this->left_pulse);
+      union {
+        double real;
+        uint64_t base;
+      } u_right_pulse;
+      u_right_pulse.real = this->right_pulse;
+      *(outbuffer + offset + 0) = (u_right_pulse.base >> (8 * 0)) & 0xFF;
+      *(outbuffer + offset + 1) = (u_right_pulse.base >> (8 * 1)) & 0xFF;
+      *(outbuffer + offset + 2) = (u_right_pulse.base >> (8 * 2)) & 0xFF;
+      *(outbuffer + offset + 3) = (u_right_pulse.base >> (8 * 3)) & 0xFF;
+      *(outbuffer + offset + 4) = (u_right_pulse.base >> (8 * 4)) & 0xFF;
+      *(outbuffer + offset + 5) = (u_right_pulse.base >> (8 * 5)) & 0xFF;
+      *(outbuffer + offset + 6) = (u_right_pulse.base >> (8 * 6)) & 0xFF;
+      *(outbuffer + offset + 7) = (u_right_pulse.base >> (8 * 7)) & 0xFF;
+      offset += sizeof(this->right_pulse);
       return offset;
     }
 
@@ -218,11 +252,41 @@ namespace vacumm_hardware
       u_right_output.base |= ((uint64_t) (*(inbuffer + offset + 7))) << (8 * 7);
       this->right_output = u_right_output.real;
       offset += sizeof(this->right_output);
+      union {
+        double real;
+        uint64_t base;
+      } u_left_pulse;
+      u_left_pulse.base = 0;
+      u_left_pulse.base |= ((uint64_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_left_pulse.base |= ((uint64_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      u_left_pulse.base |= ((uint64_t) (*(inbuffer + offset + 2))) << (8 * 2);
+      u_left_pulse.base |= ((uint64_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      u_left_pulse.base |= ((uint64_t) (*(inbuffer + offset + 4))) << (8 * 4);
+      u_left_pulse.base |= ((uint64_t) (*(inbuffer + offset + 5))) << (8 * 5);
+      u_left_pulse.base |= ((uint64_t) (*(inbuffer + offset + 6))) << (8 * 6);
+      u_left_pulse.base |= ((uint64_t) (*(inbuffer + offset + 7))) << (8 * 7);
+      this->left_pulse = u_left_pulse.real;
+      offset += sizeof(this->left_pulse);
+      union {
+        double real;
+        uint64_t base;
+      } u_right_pulse;
+      u_right_pulse.base = 0;
+      u_right_pulse.base |= ((uint64_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_right_pulse.base |= ((uint64_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      u_right_pulse.base |= ((uint64_t) (*(inbuffer + offset + 2))) << (8 * 2);
+      u_right_pulse.base |= ((uint64_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      u_right_pulse.base |= ((uint64_t) (*(inbuffer + offset + 4))) << (8 * 4);
+      u_right_pulse.base |= ((uint64_t) (*(inbuffer + offset + 5))) << (8 * 5);
+      u_right_pulse.base |= ((uint64_t) (*(inbuffer + offset + 6))) << (8 * 6);
+      u_right_pulse.base |= ((uint64_t) (*(inbuffer + offset + 7))) << (8 * 7);
+      this->right_pulse = u_right_pulse.real;
+      offset += sizeof(this->right_pulse);
      return offset;
     }
 
     virtual const char * getType() override { return "vacumm_hardware/VacummDiag"; };
-    virtual const char * getMD5() override { return "9a4373c87b5df46d3c49510b7322ac06"; };
+    virtual const char * getMD5() override { return "aadd4daafd910044057f442f70383aa6"; };
 
   };
 
